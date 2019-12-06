4b6e04523f16deb7d5b2bd46c59f077f2eb5267b
ivanhercaz <ivan@ivanhercaz.com>
2019-12-06 23:55:51 +0000

    General config easy to modify

--- demo/pelicanconf.py
+++ demo/pelicanconf.py
@@ -32,7 +32,41 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 # Buruma configuration
 THEME = "../"
+
+MENUITEMS_NAVBAR = (('Home', SITEURL),
+                    ('About', '/p/about.html'),
+                    ('Docs', 'p/docs.html'),
+                    ('Contact', 'p/contact.html'))
+
+MENUITEMS_NAVBAR_FEATURED = (("Featured 1", "#", "is-info"),
+                             ("Featured 2", "#", "is-primary"),
+                             ("Featured 3", "#", "is-warning"))
+
+NAVBAR_STYLE = "is-link"
+CATS_STYLE = {"Conocimiento libre": "is-primary",
+              "Divulgación científica": "is-success",
+              "Meta": "is-warning"}
+
+FOOTER = ("This website is generated thanks to <a href='https://getpelican.com'>Pelican</a>, "
+          "a content management system developed in <a href='https://python.org'>Python</a>. "
+          "It is using <a href='https://github.com/ivanhercaz/buruma'>Buruma</a>.")
+
+LICENSE = True
+LICENSE_NOTICE = ("<p><a rel='license' href='https://creativecommons.org/licenses/by-sa/4.0/'>"
+                  "<img alt='Licencia Creative Commons' style='border-width: 0'"
+                  "src='https://i.creativecommons.org/l/by-sa/4.0/88x31.png' /></a><br />This "
+                  "work is under a <a rel='license' class='license-text'"
+                  "href='https://creativecommons.org/licenses/by-sa/4.0'>Creative Commons "
+                  "Attribution-ShareAlike 4.0 International license</a>.")
+
+
+ABOUT_EXTRACT_STATUS = True
+ABOUT_EXTRACT = ("<p>Cat ipsum dolor sit amet, throw down all the stuff in the kitchen. Stand in "
+                 "front of the computer screen leave dead animals as gifts, or in the middle of "
+                 "the night i crawl onto your chest and purr gently to help you sleep or scratch "
+                 "at the door then walk away howl uncontrollably for no reason catty ipsum for "
+                 "leave buried treasure in the sandbox for the toddlers.</p>")
